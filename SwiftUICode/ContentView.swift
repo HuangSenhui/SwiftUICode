c08b90d49f5f987cb13c13b0cadd5b3a48b9ed0e
HuangSenhui <huangsenhui@live.com>
2020-05-04 23:01:21 +0800

    Initial Commit

---
+++ SwiftUICode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICode
+//
+//  Created by HuangSenhui on 2020/5/4.
+//  Copyright © 2020 H.Senhui. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
